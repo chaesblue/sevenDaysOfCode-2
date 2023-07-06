7fa79aac6bbe8e92708b2a825c9e0bbe72edc45d
Arthur <133824941+chaesblue@users.noreply.github.com>
2023-07-05 21:14:05 -0300

    Create sevenDaysOfCode-2.js

---
+++ sevenDaysOfCode-2.js
@@ -0,0 +1,7 @@
+let nome = prompt ("Digite seu nome:");
+ let idade = prompt ("Digite sua idade em numero:");
+ let curso = prompt ("Digite qual linguagem de programação deseja aprender:");
+let msgnBoasvindas = `olá ${nome}! bem vindo ao curso de ${curso} 
+vimos que vc tem ${idade} anos é uma otima  idade para começar, vamos la?!`;
+
+ alert (msgnBoasvindas)
