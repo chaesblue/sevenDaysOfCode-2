70ce460a8334a91d4e8de1d96c1c218caf19ecb1
Arthur <133824941+chaesblue@users.noreply.github.com>
2023-07-06 10:35:07 -0300

    Update sevenDaysOfCode-2.js
    adicionando condicional if e mais personalidade a mensagem

--- sevenDaysOfCode-2.js
+++ sevenDaysOfCode-2.js
@@ -2,6 +2,18 @@ let nome = prompt ("Digite seu nome:");
  let idade = prompt ("Digite sua idade em numero:");
  let curso = prompt ("Digite qual linguagem de programação deseja aprender:");
 let msgnBoasvindas = `olá ${nome}! bem vindo ao curso de ${curso} 
-vimos que vc tem ${idade} anos é uma otima  idade para começar, vamos la?!`;
 
- alert (msgnBoasvindas)
+vimos que vc tem ${idade} anos, é uma otima  idade para começar, vamos la?!`;
+
+let msgnBoasvindas2 = `olá ${nome}! bem vindo ao curso de ${curso} 
+vimos que vc tem ${idade} anos, é uma otima  idade para inovar, vamos la?!`;
+
+
+if (idade<30){
+
+alert (msgnBoasvindas)
+
+}else{
+
+    alert(msgnBoasvindas2)
+}
